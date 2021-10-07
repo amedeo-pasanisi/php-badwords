b3e883f84e1219699537262f51b9c00a07d26088
amedeo <amedeopasanisi97@gmail.com>
2021-10-07 16:42:25 +0200

    censura parole, fine esercizio

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 $paragrafo = "Qualsiasi storia delle opere di ingegno si riduce a un elenco di fatti miracolosi, se ne ignoriamo il contesto.<br/> La Storia, anche quando c'è di mezzo il talento, deve essere sempre storia sociale,<br/> altrimenti non parla di niente. ";
+$censura = $_GET["censura"];
+$paragrafoCensurato = str_replace($censura, "***", $paragrafo);
 ?>
 
-<p><?php echo $paragrafo, strlen($paragrafo); ?> caratteri</p>
+<p><?php echo $paragrafoCensurato, strlen($paragrafo); ?> caratteri</p>
